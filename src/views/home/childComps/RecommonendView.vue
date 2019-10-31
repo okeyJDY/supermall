5f7b6315f3c29af43dc47f4bbbd664d75f67dccb
okeyJDY <okeyjdy@163.com>
2019-10-31 09:04:44 +0800

    第一次提交

---
+++ src/views/home/childComps/RecommonendView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <span>{{item.title}}</span>
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "RecommonendView",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return [];
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+    padding: 10px 0 20px 0;
+    border-bottom: 10px solid #eeeeee;
+  }
+  .recommend-item {
+    flex: 1;
+  }
+  .recommend-item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
